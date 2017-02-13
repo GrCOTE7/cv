c38b783d49d498af43e319b85bb4835b381ffaad
Lionel COTE <grcote7@gmail.com>
2017-02-13 03:18:49 +0100

    insertion liste des pays dans inscription

--- inscription.php
+++ inscription.php
@@ -32,7 +32,22 @@
 
         <tr>
             <td>Votre pays</td>
-            <td><input type="TEXT" name="pays"></td>
+            <td><?php
+                include_once 'inc/connection.inc.php';
+
+                $sql = 'SELECT * FROM pays ORDER BY nom';
+
+                $resultat = mysqli_query($dblink, $sql);
+
+                echo '<select name="pays">';
+                echo '<option value="" selected>Choisissez votre pays</option>';
+                while ($rang = mysqli_fetch_array($resultat)) {
+                    $code = $rang['code'];
+                    $nom  = $rang['nom'];
+                    echo '<option value="' . $code . '">' . $nom . '</option>' . "\n";
+                }
+                ?>
+            </td>
         </tr>
 
         <tr>
