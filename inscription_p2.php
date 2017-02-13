d5f7cddcc1ddbdd0fd1949780b39389562a17698
Lionel COTE <grcote7@gmail.com>
2017-02-13 03:45:49 +0100

    Suppr var_dump

--- inscription_p2.php
+++ inscription_p2.php
@@ -1,17 +1,12 @@
-<pre>
-<?php
-print_r($_POST);
-?>
-</pre>
 <?php
 
 include_once 'inc/connection.inc.php';
 
-$prenom            = $_POST['prenom'];
-$email             = $_POST['email'];
-$code_postal       = $_POST['code_postal'];
-$ville             = $_POST['ville'];
-$pays              = $_POST['pays'];
+$prenom           = $_POST['prenom'];
+$email            = $_POST['email'];
+$code_postal      = $_POST['code_postal'];
+$ville            = $_POST['ville'];
+$pays             = $_POST['pays'];
 $date_inscription = date('Y-m-d');
 
 $sql = "INSERT INTO membres (prenom, code_postal, ville, pays, email, date_inscription) VALUES ('$prenom', '$code_postal', '$ville', '$pays', '$email', '$date_inscription')";
